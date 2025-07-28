0b57c2f4c8e38a9c3a46713cf0ba265e4ef4b76e
roulendz <rolands.zeltins@inbox.lv>
2025-07-28 23:48:29 +0300

    Added search_synonym field to lovata_shopaholic_offers table.

---
+++ updates/update_table_offers.php
@@ -0,0 +1,40 @@
+<?php namespace Lovata\SearchShopaholic\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+/**
+ * Class UpdateTableProduct
+ * @package Lovata\SearchShopaholic\Updates
+ */
+class UpdateTableProduct extends Migration
+{
+    /**
+     * Apply migration
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('lovata_shopaholic_offers') || Schema::hasColumn('lovata_shopaholic_offers', 'search_synonym')) {
+            return;
+        }
+
+        Schema::table('lovata_shopaholic_offers', function (Blueprint $obTable) {
+            $obTable->text('search_synonym')->nullable();
+        });
+    }
+
+    /**
+     * Rollback migration
+     */
+    public function down()
+    {
+        if (!Schema::hasTable('lovata_shopaholic_offers') || !Schema::hasColumn('lovata_shopaholic_offers', 'search_synonym')) {
+            return;
+        }
+
+        Schema::table('lovata_shopaholic_offers', function (Blueprint $obTable) {
+            $obTable->dropColumn(['search_synonym']);
+        });
+    }
+}
